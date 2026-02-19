a0c4043e7343779b7aecb4104f9e82d98aa672d9
jun <50354282+wjdwns@users.noreply.github.com>
2026-02-20 00:01:32 +0900

    타겟 추적 State Tree 리팩터링

--- Source/GearOfThree/EnemyNPC/Public/WeaponComponent.h
+++ Source/GearOfThree/EnemyNPC/Public/WeaponComponent.h
@@ -1,9 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GearWeaponBase.h" // AGearWeaponBase를 사용하기 위해 필요할 수 있습니다.
 #include "WeaponComponent.generated.h"
 
 USTRUCT(BlueprintType)
@@ -12,7 +11,7 @@ struct FWeaponData
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere)
-	TSubclassOf<class AGearWeaponBase> WeaponClass; // 장착할 무기 클래스
+	TSubclassOf<AGearWeaponBase> WeaponClass; 
 };
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -21,15 +20,38 @@ class GEAROFTHREE_API UWeaponComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:
-	
 	UWeaponComponent();
-	
-	void Fire(); // 이제 내부에서 CurrentWeapon->Fire()만 호출
-	// 무기를 소환하고 손 소켓에 부착하는 함수
+    
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 	void EquipWeapon(TSubclassOf<AGearWeaponBase> WeaponClass, FName SocketName);
 
+	void Fire(); 
+
+
+	// 탄창 및 장전 시스템
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon|Ammo")
+	int32 MaxAmmo = 20; // 최대 탄창
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Ammo")
+	int32 CurrentAmmo; // 현재 남은 총알
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon|Ammo")
+	float ReloadTime = 2.0f; // 장전 소요 시간
+
+	bool bIsReloading = false; // 현재 장전 중인지 여부
+
+	// 무기 자체의 연사 속도 제어
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon|Stats")
+	float FireRate = 0.2f; // 0.2초에 한 발씩 발사
+
 private:
+	float LastFireTime = 0.0f; // 마지막으로 쏜 시간 기억
+
+	// 장전 타이머 관리를 위한 함수와 핸들
+	void StartReload();
+	void EndReload();
+	FTimerHandle ReloadTimerHandle;
+
 	UPROPERTY()
 	TObjectPtr<AGearWeaponBase> CurrentWeapon;
 };
