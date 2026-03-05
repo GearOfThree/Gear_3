dbd58a9f4e6db113b7f5b362195530cbbbd249c7
rottencarrot0 <qpwo3824@gmail.com>
2026-03-05 17:38:10 +0900

    OnRestart()시 정상 동작하도록 설정 전환

--- Source/GearOfThree/EnemyNPC/Private/GameOverUI.cpp
+++ Source/GearOfThree/EnemyNPC/Private/GameOverUI.cpp
@@ -16,6 +16,26 @@ void UGameOverUI::NativeOnInitialized()
 
 void UGameOverUI::OnRestart()
 {
+	APlayerController* PC = GetOwningPlayer();
+	
+	if (PC)
+	{
+		// 1) UI 포커스/입력모드 되돌리기
+		FInputModeGameOnly Mode;
+		PC->SetInputMode(Mode);
+
+		// 2) 커서 숨기기
+		PC->SetShowMouseCursor(false);
+
+		// 3) 일시정지 해제
+		PC->SetPause(false);
+
+		// (선택) 남아있는 입력/포커스 정리
+		PC->FlushPressedKeys();
+	}
+	
+	RemoveFromParent();
+	
 	FString LevelName = UGameplayStatics::GetCurrentLevelName(GetWorld());
 	UGameplayStatics::OpenLevel(GetWorld(), FName(*LevelName));
 }
